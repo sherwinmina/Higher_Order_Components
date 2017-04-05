68b5177b83d868bd4bfa68b0a297752e678ec0e5
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-05 11:35:21 -0700

    fixed export defaults

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { Component } from 'react';
+import React, { Component } from 'react';
 
 import Header from './header';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <Header />
+      <div>
+        <Header />
+      </div>
     );
   }
 }
