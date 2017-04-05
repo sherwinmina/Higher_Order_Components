58e3c62d424db803f9874552d6f82ac252a00033
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-05 11:12:50 -0700

    styled navbar

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <nav className="navbar navbar-light"></nav>
+      <nav className="navbar navbar-light">
+        <ul className="nav navbar-nav">
+          <li className="nav-item"></li>
+          <li className="nav-item"></li>
+          <li className="nav-item"></li>
+        </ul>
+      </nav>
     );
   }
 }
