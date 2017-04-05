b976418ee64598dc005ba1d2cf13ed0b0f3a7511
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-05 11:16:26 -0700

    Added Link component

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 class Header extends Component {
   render() {
     return (
       <nav className="navbar navbar-light">
         <ul className="nav navbar-nav">
-          <li className="nav-item"></li>
-          <li className="nav-item"></li>
-          <li className="nav-item"></li>
+          <li className="nav-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/resources">Resources</Link>
+          </li>
+          <li className="nav-item">
+            {this.authButton()}
+          </li>
         </ul>
       </nav>
     );
