88c5263ddc43837b2fa68f89f12e867d110e6596
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-05 12:16:20 -0700

    updated routes

--- src/components/resources.js
+++ src/components/resources.js
@@ -4,6 +4,11 @@ export default () => {
  return (
   <div>
     Super Special Recipe
+    <ul>
+      <li>1 cup of Peas</li>
+      <li>1 cup butter</li>
+      <li>1 cup flour</li>
+    </ul>
   </div>
  )
 };
