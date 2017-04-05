6b8da9d0b8e6248f20fcc2107d3b8cb971373509
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-05 11:49:41 -0700

    Added resources component

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, browserHistory } from 'react-router';
 
 import App from './components/app';
+import Resources from './components/resources';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
@@ -12,7 +13,8 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
-      <Route path="/" component={App}>  
+      <Route path="/" component={App}>
+        <Route path="resources" component={Resources} />  
       </Route>
     </Router>
   </Provider>
